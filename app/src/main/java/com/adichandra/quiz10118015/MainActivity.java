edd18faa20e5d763549759685e82d76042401265
adichnadra <adichandra019@gmail.com>
2021-05-29 12:55:46 +0700

    initiation

---
+++ app/src/main/java/com/adichandra/quiz10118015/MainActivity.java
@@ -0,0 +1,22 @@
+package com.adichandra.quiz10118015;
+
+/*
+ * NIM   : 10118015
+ * NAMA  : Adi Chandra Nugraha
+ * KELAS : IF1
+ * TGL   : 29/05/2021
+ * Deskripsi : Membuat MainActivity
+ * */
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
